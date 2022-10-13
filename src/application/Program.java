3005c70432fd3be05719aee4d946a8bd84dbc89b
Yago Alexandre <goltarayago@gmail.com>
2022-10-13 20:23:33 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Marketing");
-		System.out.println(obj);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Department department = new Department(1, "Marketing");
+		
+		Seller seller = new Seller(2, "Yago", "goltara@mail.com", new Date(), 20000.0, department);
+		
+		System.out.println(seller);
 	}
 }
